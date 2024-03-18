22904c6580864e8fa6df2c910beac3baf14c5eeb
Yoel Leghesse <114878101+yoelleghesse@users.noreply.github.com>
2024-03-18 13:47:07 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+  print("Get request string")
+  return render_template('index.html')
+
+
+@app.route('/', methods = ['POST'])
+def home_post():
+  dim1 = request.form['first_dim']
+  dim2 = request.form['second_dim']
+  dim3 = request.form['third_dim']
+  volume = float(dim1) * float(dim2) * float(dim3)
+  print()
+
+  print("Get post request")
+  return render_template('index.html', output=volume, dim_1=dim1, dim_2=dim2, dim_3=dim3)
+
+app.run(host='0.0.0.0')
